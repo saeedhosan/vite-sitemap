fff80114094f6cc0a93dad78e4a41310e154428c
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-10-09 06:28:43 +0600

    chor(package): update package.json

--- index.js
+++
@@ -1,9 +0,0 @@
-
-import { format } from "date-fns";
-
-
-const today = new Date()
-
-console.log(format(today, "yyyy-MM-dd'T'HH:mm:ss'Z'"))
-console.log('\n');
-console.log((new Date()).toISOString());
